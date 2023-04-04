052177e9c4587019e19ad5922ad4cf7cf7bab234
Lysenko <lysenkoden@gmail.com>
2023-04-04 21:52:15 +0200

    add try-catch block

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-//https://dev.to/ramonak/javascript-how-to-access-the-return-value-of-a-promise-object-1bck
 //https://www.youtube.com/watch?v=Veaz0BMMOWA&list=PLenwk9TUJzJ41x_7o3Z5laAngAuuqfpQW&index=25
+//https://dev.to/ramonak/javascript-how-to-access-the-return-value-of-a-promise-object-1bck
 // ==============================================================================================================================================================================================
 const ulElement = document.querySelector(".posts-list");
 const arr = fetch("https://jsonplaceholder.typicode.com/posts")
@@ -9,26 +9,28 @@ const arr = fetch("https://jsonplaceholder.typicode.com/posts")
   });
 
 const myPosts = async () => {
-  const posts2 = await arr;
-  let result = posts2.map(function (elements) {
-    return elements;
-  });
-  result.forEach((element) => {
-    ulElement.insertAdjacentHTML(
-      "afterbegin",
-      `<li class="posts-list-item"><span class="post-index">${
-        element.id
-      }</span><h4 class="post-title">${
-        element.title
-      }</h4><p class="post-text">${
-        element.body
-      }</p><a href="#" class="post-link">${"Read more"}</a></li>`
-    );
-  });
+  try {
+    const posts2 = await arr;
+    let result = posts2.map(function (elements) {
+      return elements;
+    });
+    result.forEach((elem) => {
+      ulElement.insertAdjacentHTML(
+        "afterbegin",
+        `<li class="posts-list-item"><span class="post-index">${
+          elem.id
+        }</span><h4 class="post-title">${elem.title}</h4><p class="post-text">${
+          elem.body
+        }</p><a href="#" class="post-link">${"Read more"}</a></li>`
+      );
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 myPosts();
 
-// author`s version==============================================================================================================================================================================================
+// Vitaly`s version==============================================================================================================================================================================================
 // fetch("https://jsonplaceholder.typicode.com/posts")
 //   .then((response) => response.json())
 //   .then((json) => {
